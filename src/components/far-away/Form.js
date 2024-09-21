443895126a93592be6bf4ea311c6e8a90db18043
iampeterjohn <peterjeche442@gmail.com>
2024-09-21 05:55:19 +0100

    form submission

--- src/components/far-away/Form.js
+++ src/components/far-away/Form.js
@@ -1,7 +1,25 @@
 export default function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
     <div>
       <h3>what do you need for your 😍 trip</h3>
+      <form onSubmit={handleSubmit}>
+        <select>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option num={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+
+        <input type="text" placeholder="item..." />
+
+        <button>add</button>
+      </form>
     </div>
   );
 }
